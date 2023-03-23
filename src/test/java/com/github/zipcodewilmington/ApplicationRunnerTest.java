d0b1ec0b71948837703519c2b9436b6fe22608b7
hiepnguyen90 <johnhiepnguyen1@gmail.com>
2023-03-23 16:06:15 -0400

    tests, casino, accountmanager

--- src/test/java/com/github/zipcodewilmington/ApplicationRunnerTest.java
+++ src/test/java/com/github/zipcodewilmington/ApplicationRunnerTest.java
@@ -1,21 +1,54 @@
 package com.github.zipcodewilmington;
 
+import com.github.zipcodewilmington.casino.CasinoAccount;
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Created by leon on 7/21/2020.
  */
 public class ApplicationRunnerTest {
-    @Test
-    public void test() { // TODO - replace boiler-plate logic with business logic
-        // given
-        Runnable runnable = new Casino();
+    @org.junit.jupiter.api.Test
+    void deposit() {
+        //given
+        CasinoAccount hiep = new CasinoAccount("ZipCode", "rocks", 1000);
 
-        // when
-        runnable.run();
+        //when
+        hiep.deposit(500);
+        int expected = 1500;
 
-        // then
-        Assert.assertNotNull(runnable.toString());
+        //then
+        Assert.assertEquals(expected, hiep.getBalance());
     }
+
+    @org.junit.jupiter.api.Test
+    void getUserName() {
+        //given
+        CasinoAccount hiep = new CasinoAccount("ZipCode", "rocks", 1000);
+
+        //when
+
+        //then
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPassword() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAccount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+    }
+
+    @Test
+    void setBalance() {
+    }
+
 }
