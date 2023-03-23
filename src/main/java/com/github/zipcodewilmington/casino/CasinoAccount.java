ca78f8a581e85129e3e393a7b47385785fb8673e
hiepnguyen90 <johnhiepnguyen1@gmail.com>
2023-03-22 22:08:44 -0400

    first commit

--- src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
+++ src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
@@ -1,9 +1,58 @@
 package com.github.zipcodewilmington.casino;
 
+import java.util.Scanner;
+
 /**
  * Created by leon on 7/21/2020.
  * `ArcadeAccount` is registered for each user of the `Arcade`.
  * The `ArcadeAccount` is used to log into the system to select a `Game` to play.
  */
 public class CasinoAccount {
+
+    Integer balance;
+    String userName;
+    String password;
+    char input1 = 'Z';
+    Scanner input = new Scanner(System.in);
+
+    public CasinoAccount(){}
+
+
+    public CasinoAccount(String username, String password, Integer balance){
+        this.userName = username;
+        this.password = password;
+        this.balance = balance;
+
+    }
+
+    public void deposit(int deposit){
+        this.balance = balance + deposit;
+    }
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public CasinoAccount getAccount(){
+        CasinoAccount account = new CasinoAccount(userName, password, balance);
+        return account;
+    }
+
+    public String toString(){
+        return userName + "," + password + "," + balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+
+
 }
+
